20e7b7b385c4fe661014be98f039cc23465db963
eugeneburdukov <burdukov.eugene@gmail.com>
2024-01-27 02:23:58 +0200

    first commit

--- src/main/java/Calculator.java
+++ src/main/java/Calculator.java
@@ -1,2 +1,6 @@
-package PACKAGE_NAME;public class Calculator {
+public class Calculator {
+
+    public static int multiple(int n, int m) {
+        return n * m;
+    }
 }
--- src/test/java/CalculatorTest.java
+++ src/test/java/CalculatorTest.java
@@ -1,2 +1,25 @@
-package PACKAGE_NAME;public class CalculatorTest {
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+public class CalculatorTest {
+
+    @Test
+    @DisplayName("2 x 2 = 4")
+    void test1() {
+        Assertions.assertEquals(4, Calculator.multiple(2, 2));
+    }
+
+    @Test
+    @DisplayName("2 x 3 = 6")
+    void test2() {
+        Assertions.assertEquals(6, Calculator.multiple(3, 2));
+    }
+
+    @Test
+    @DisplayName("2 x 6 = 12")
+    void test3() {
+        Assertions.assertEquals(13, Calculator.multiple(2, 6));
+    }
+
 }
