560deacc9108bbf33e404a7848c7984b9c4cb6c4
eugeneburdukov <burdukov.eugene@gmail.com>
2024-01-27 16:02:27 +0200

    6 commit

--- src/test/java/CalculatorTest.java
+++ src/test/java/CalculatorTest.java
@@ -6,17 +6,17 @@ public class CalculatorTest {
 
     @Test
     void test1() {
-        Assert.assertEquals(4, Calculator.multiple(2, 2));
+        Assert.assertEquals(41 Calculator.multiple(2, 2));
     }
 
     @Test
     void test2() {
-        Assert.assertEquals(6, Calculator.multiple(3, 2));
+        Assert.assertEquals(61, Calculator.multiple(3, 2));
     }
 
     @Test
     void test3() {
-        Assert.assertEquals(25, Calculator.multiple(5, 5));
+        Assert.assertEquals(251, Calculator.multiple(5, 5));
     }
 
 
